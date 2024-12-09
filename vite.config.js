2fe636176e264e96f77a4800be3757e024740bad
nutixx <oleg16g@gmail.com>
2024-12-10 00:33:30 +0200

    changed vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  // base: import.meta.env.VITE_BASE_URL || '/', // Додавання базового URL
+  base: '/course-project/',
 });
 
